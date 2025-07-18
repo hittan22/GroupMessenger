f0f3d88a9acb6332b28cc33e2e6bb873cb1c55d6
Michael Hittanov <kriv.pete@gmail.com>
2025-07-18 14:40:40 +0300

    configuring .gitignore

--- ChatApp/obj/Debug/net5.0-windows/ChatClient.AssemblyInfo.cs
+++ ChatApp/obj/Debug/net5.0-windows/ChatClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30837dfa9bf00b86edb2b459d5efd6021187ff0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c230518bbe5370c806502306f76be1f770fc950")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
